f39bf8797d76b464fcc2ae25cb070b3805176e91
Subhas R <subhas-tt0445@subhas-tt0445.csez.zohocorpin.com>
2024-09-06 14:43:53 +0530

    oim

---
+++ main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+class Main {
+
+    static void fun(Object o) {
+        System.out.println(o);
+    }
+
+    public static void main(String... arg) {
+        fun("hie");
+        fun(123);
+
+    }
+
+}
